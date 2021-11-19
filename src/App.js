a5c22069151e5ac4be32578a0726535e941ae394
Ilya Sushkov <pyh3@yandex.ru>
2021-11-19 07:21:44 +0300

    lesson_1 start

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App({name}) {
+  return (
+    <div className="App">
+      <header className="App-header">
+<div>{name}</div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
